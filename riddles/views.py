354f2d67d1f79bd86605ccfcb44980707ce461fc
BCanary <mike_channel2@mail.ru>
2020-09-17 14:17:26 +0300

    fc

---
+++ riddles/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Book
+# Create your views here.
+COUNT_OF_BOOKS_ON_PAGE = 24
+def index(request):
+	page = request.GET.get("page", "0")
+	all_objects = Book.objects.all()
+	objects = all_objects[0+COUNT_OF_BOOKS_ON_PAGE*int(page):COUNT_OF_BOOKS_ON_PAGE+COUNT_OF_BOOKS_ON_PAGE*int(page)]
+	books_count = len(objects)
+	pages_count = len(all_objects)/COUNT_OF_BOOKS_ON_PAGE
+	data = {"pages_count": pages_count, "books": objects, "books_count": books_count}
+	return render(request, "riddles/index.html", context=data)
