eddb347f38ed694e29226bf7516613e015f6525b
BCanary <mike_channel2@mail.ru>
2020-09-20 17:57:27 +0300

    reg, filters

--- riddles/models.py
+++ riddles/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Book(models.Model):
-	name = models.CharField(max_length=25, default="No name")
-	author = models.CharField(max_length=15, default="Anonymus")
+	name = models.CharField(max_length=25, default="")
+	author = models.CharField(max_length=15, default="")
+	description = models.CharField(max_length=1000, default="")
 	image_id = models.CharField(max_length=10, default="0")
 	image_format = models.CharField(max_length=6, default="jpg")
--- riddles/views.py
+++ riddles/views.py
@@ -5,15 +5,29 @@ COUNT_OF_BOOKS_ON_PAGE = 24
 def index(request):
 	page = request.GET.get("page", "0")
 	search = request.GET.get("search", "")
+	s_author = request.GET.get("search-author", "off")
+	s_name = request.GET.get("search-name", "off")
+	s_description = request.GET.get("search-description", "off")
 
 	if search == "":
 		all_objects = Book.objects.all()
 	else:
-		objects_by_name = Book.objects.filter(name__icontains=search)
-		objects_by_author = Book.objects.filter(author__icontains=search)
+		objects_by_name = []
+		objects_by_author = []
+		objects_by_description = []
+
+		if s_name == "on":
+			objects_by_name = Book.objects.filter(name__icontains=search)
+		if s_author == "on":
+			objects_by_author = Book.objects.filter(author__icontains=search)
+		if s_description == "on":
+			objects_by_description = Book.objects.filter(description__icontains=search)
+
 		all_objects = []
 		all_objects.extend(objects_by_name)
 		all_objects.extend(objects_by_author)
+		all_objects.extend(objects_by_description)
+
 	objects = all_objects[0+COUNT_OF_BOOKS_ON_PAGE*int(page):COUNT_OF_BOOKS_ON_PAGE+COUNT_OF_BOOKS_ON_PAGE*int(page)]
 	books_count = len(objects)
 	pages_count = len(all_objects)/COUNT_OF_BOOKS_ON_PAGE
