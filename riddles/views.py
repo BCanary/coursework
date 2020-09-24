60d26cf184a108770de687570b9d722d508c377a
BCanary <mike_channel2@mail.ru>
2020-09-24 22:34:35 +0300

    Update new

--- riddles/views.py
+++ riddles/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 from .models import Book
 # Create your views here.
 COUNT_OF_BOOKS_ON_PAGE = 24
+def toCheckbox(checked):
+	if checked == "on":
+		return "checked"
+	else:
+		return ""
+
 def index(request):
 	page = request.GET.get("page", "0")
 	search = request.GET.get("search", "")
+
 	s_author = request.GET.get("search-author", "off")
 	s_name = request.GET.get("search-name", "off")
 	s_description = request.GET.get("search-description", "off")
@@ -28,10 +35,18 @@ def index(request):
 		all_objects.extend(objects_by_author)
 		all_objects.extend(objects_by_description)
 
+	filters = {
+		"name": toCheckbox(s_name),
+		"author": toCheckbox(s_author),
+		"description": toCheckbox(s_description),
+		"search": search,
+		"page": page
+	}
+
 	objects = all_objects[0+COUNT_OF_BOOKS_ON_PAGE*int(page):COUNT_OF_BOOKS_ON_PAGE+COUNT_OF_BOOKS_ON_PAGE*int(page)]
 	books_count = len(objects)
 	pages_count = len(all_objects)/COUNT_OF_BOOKS_ON_PAGE
-	data = {"pages_count": pages_count, "books": objects, "books_count": books_count}
-	
+	data = {"pages_count": pages_count, "books": objects, "books_count": books_count, "filters": filters}
+
 	RENDER = render(request, "riddles/index.html", context=data)
 	return RENDER
